92500cde8144bbcf23109a29ba9badd740ad9ace
lpolyak68 <lpolyk_68@yahoo.com>
2018-02-01 13:28:48 -0800

    Commiting Files

---
+++ A1ToCatalogMenuLoader/Containers/MenuFileContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace A1ToCatalogMenuLoader.Containers
+{
+	public class MenuFileContainer
+	{
+		public string RestNumber { get; set; }
+		public DateTime FileDate { get; set; }
+		public FileInfo FileInstance { get; set; }
+	}
+}
---
+++ OpComponentsLoader/MenuItemContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpComponentsLoader
+{
+	public class MenuItemContainer
+	{
+		public int AiiNum { get; set; }
+		public int BrandModMenuCatalogItemID { get; set; }
+	}
+}
